c92ce45845e6f6c07f18e47cb567ac0419eaf69d
BornToBeRoot <16019165+BornToBeRoot@users.noreply.github.com>
2022-12-08 22:51:48 +0100

    Solve part 2 & improve performance

--- 2022/08/day08.ps1
+++ 2022/08/day08.ps1
@@ -13,52 +13,76 @@ foreach ($Line in Get-Content -Path $PSScriptRoot\day08_input.txt) {
     [void]$Row.Add($Column)
 }
 
+# Ignore borders, so we can ignore the first and last row and column 
+# when iterating over the array
 $VisibleTrees = ($Row.Count * 2) + ($Row[0].Count * 2) - 4
+
+$TreeScenicScore = 0
 
 # Go through each row
 for ($i = 1; $i -lt $Row.Count - 1; $i++) {
     # Go through each column
     for ($y = 1; $y -lt $Row[$i].Count - 1; $y++) {
         $CurrentTree = $Row[$i][$y]
-        #Write-Host "Current tree is [$i,$y] with value $CurrentTree"
+        Write-Host "Current tree is [$i,$y] with value $CurrentTree"
 
         # Left
         $LeftVisible = $true
-        $Row[$i][0..($y - 1)] | ForEach-Object {
-            if ($_ -ge $CurrentTree) {
+        $LeftCount = 0
+        foreach($l in $Row[$i][($y - 1)..0]) {
+            $LeftCount++
+            if ($l -ge $CurrentTree) {
                 $LeftVisible = $false
+                break
             }
-        }
+        }         
         
         # Right
         $RightVisible = $true
-        $Row[$i][($y + 1)..$($Row[$y].Count)] | ForEach-Object {
-            if ($_ -ge $CurrentTree) {
+        $RightCount = 0
+        foreach($r in $Row[$i][($y + 1)..$($Row[$y].Count)]) {
+            $RightCount++
+            if ($r -ge $CurrentTree) {                
                 $RightVisible = $false
+                break
             }
         }
 
         # Top
         $TopVisible = $true
-        $Row[0..($i - 1)] | ForEach-Object { $_[$y] } | ForEach-Object {
-            if ($_ -ge $CurrentTree) {
+        $TopCount = 0
+        foreach($t in $Row[($i - 1)..0]) {                 
+            $TopCount++
+            if($($t[$y]) -ge $CurrentTree)   {
                 $TopVisible = $false
-            }
+                break
+            }         
         }
         
         # Bottom
         $BotVisible = $true
-        $Row[($i + 1)..$($Row[$i].Count)] | ForEach-Object { $_[$y] } | ForEach-Object {
-            if ($_ -ge $CurrentTree) {
+        $BotCount = 0
+        foreach($b in $Row[($i + 1)..$($Row[$i].Count)]) {            
+            $BotCount++            
+            if($($b[$y]) -ge $CurrentTree)   {
                 $BotVisible = $false
-            }
+                break
+            }            
         }
 
-        # Check if visible
+        # Check if tree is visible
         if ($LeftVisible -or $TopVisible -or $RightVisible -or $BotVisible) {
             $VisibleTrees++
+        }
+
+        # Check scenic score        
+        $CurrentTreeScenicScore = $TopCount * $LeftCount * $BotCount * $RightCount 
+        
+        if ($CurrentTreeScenicScore -gt $TreeScenicScore) {
+            $TreeScenicScore = $CurrentTreeScenicScore
         }
     }    
 }
 
-$VisibleTrees
+Write-Host "Part 1: $VisibleTrees"
+Write-Host "Part 2: $TreeScenicScore"
